7b5cc7b47e77f43716b7aedbf1a4527dbf649000
Lansen Zhao <lansenz@uci.edu>
2017-11-13 13:11:19 -0800

    initial commit

---
+++ js/myscript.js
@@ -0,0 +1,17 @@
+// <script>
+// 	function visitPage(){
+// 		window.location = 'http://www.google.com';
+// 	}
+
+// </script>
+// <button onclick="visitPage();">visitPage</button>
+
+// <script>
+//     function visitPage(){
+//         window.location='http://www.example.com';
+//     }
+//     <button onclick="visitPage();">Visit Page Now </button>
+// </script>
+#demo{
+	width: 200px;
+}
